2014bc6780b02d849e037d3dce877787c065e389
kurnia muhamad <omenkzz@gmail.com>
2014-06-29 16:39:21 +0700

    setup book's data model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create!(isbn:'978-3-16-148410-0', title: 'rails 4 tutorial', author:'kurnia muhamad',publisher: 'majumundur')
+Book.create!(isbn:'897-5-21-982347-0', title: 'angular.js tutorial', author:'sastra djaya',publisher: 'majumundur')
+Book.create!(isbn:'456-7-16-093489-0', title: 'python 3 tutorial', author:'micahel heart',publisher: 'majumundur')
+Book.create!(isbn:'977-9-45-283685-0', title: 'node.js  tutorial', author:'david HH',publisher: 'majumundur')
